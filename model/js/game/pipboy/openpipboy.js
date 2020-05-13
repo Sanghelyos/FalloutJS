459d55e208e3718b160f4859e1156a3ad6f13854
Sanghelyos <pecheux@simplon-charleville.fr>
2020-05-13 13:31:11 +0200

    Pipboy improvements and credits

--- model/js/game/pipboy/openpipboy.js
+++ model/js/game/pipboy/openpipboy.js
@@ -1,15 +1,27 @@
 $('#pipboyOpener').click(function(){
     var wWidth = $('#pipboyInterface').css('width');
     if (wWidth > '0vh'){
-        $('#pipboyInterface').css('width', '0vw');
+        pboyExit.play();
         pboyHum.pause();
         pboyHum.currentTime = 0;
-        pboyExit.play();
+        $("#pipboyInterface").css('background-image','url("")');
+        $("#pipboyInterfaceContent").css('visibility', 'hidden');
+        $("#pipboyInterface").animate({width:'0vw'},200);
+        //$('#pipboyInterface').css('width', '0vw');
+        
+        
     }
     else{
-        $('#pipboyInterface').css('width', '70vw');
         pboyAccess.play();
-        pboyHum.play();
-        pboyHum.loop = true;
+        $("#pipboyInterface").animate({width:'70vw'},200);
+        setTimeout(function(){
+            $("#pipboyInterface").css('background-image','url("assets/img/pipboy/pboyscreen.png")');
+            $("#pipboyInterfaceContent").css('visibility', 'visible');
+            pboyHum.play();
+            pboyHum.loop = true;
+        },200);
+        
+        //$('#pipboyInterface').css('width', '70vw');
+        
     }
 });
