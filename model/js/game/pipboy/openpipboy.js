ad054fba6826bc8c7644f382dedd2a40d3792ab7
Sanghelyos <pecheux@simplon-charleville.fr>
2020-05-15 16:19:53 +0200

    Improved pip-boy

--- model/js/game/pipboy/openpipboy.js
+++ model/js/game/pipboy/openpipboy.js
@@ -4,8 +4,9 @@ $('#pipboyOpener').click(function(){
         pboyExit.play();
         pboyHum.pause();
         pboyHum.currentTime = 0;
-        $("#pipboyInterface").css({'background-image':'url("")',
-                                    'border-right':'none'});
+        // $("#pipboyInterface").css('background-image','url("")');
+        $("#pipboyInterface").css('margin-right','0vw');
+        $('#pipboyInterface').css('background-image', 'radial-gradient(#111, #111)')
         $("#pipboyInterfaceContent").css('visibility', 'hidden');
         $("#pipboyInterface").animate({width:'0vw'},200);
         //$('#pipboyInterface').css('width', '0vw');
@@ -16,8 +17,9 @@ $('#pipboyOpener').click(function(){
         pboyAccess.play();
         $("#pipboyInterface").animate({width:'92vw'},200);
         setTimeout(function(){
-            $("#pipboyInterface").css({'background-image':'url("assets/img/pipboy/pboyscreen.png")', 
-                                        'border-right':'4vw solid #A9765F'});
+            // $("#pipboyInterface").css('background-image','url("assets/img/pipboy/pboyscreen.png")');
+            $("#pipboyInterface").css('margin-right','4vw');
+            $('#pipboyInterface').css('background-image', 'radial-gradient(#444, #111)')
             $("#pipboyInterfaceContent").css('visibility', 'visible');
             pboyHum.play();
             pboyHum.loop = true;
